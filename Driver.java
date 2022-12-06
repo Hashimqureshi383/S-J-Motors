6df55e2be22cdf54e56516f120e9fec6f0e74902
Hashimqureshi383 <85284853+Hashimqureshi383@users.noreply.github.com>
2022-12-07 00:15:18 +0500

    Update Driver.java
    Outlet Admin Functionalities Completed

--- Driver.java
+++ Driver.java
@@ -118,4 +118,234 @@ public class Driver
         }
 
     }
+    private static void AdminMenu() {
+        byte op = 1;
+
+        while (op != 6) {
+            System.out.print("Welcome Admin\n\n");
+            System.out.print("1. Add employee\n");
+            System.out.print("2. Remove employee\n");
+            System.out.print("3. Assign Task\n");
+            System.out.print("4. Show Jobs\n");
+            System.out.print("5. Show Employees\n");
+            System.out.print("6. return\n");
+            Scanner input = new Scanner(System.in);
+            op = input.nextByte();
+
+            if (op == 1) {
+                addEmployee();
+            }
+
+            else if (op == 2) {
+                removeEmployee();
+            }
+
+            else if (op == 3) {
+                AssignTask();
+            }
+
+            else if (op == 3) {
+                AllJobs();
+            }
+
+            else if (op == 3) {
+                AllEmployee();
+            }
+
+        }
+    }
+
+    /*
+     * User newUser = new User();
+     * System.out.println("Enter CNIC= ");
+     * newUser.setCnic(input.nextInt());
+     * System.out.println("Enter Name= ");
+     * newUser.setName(input.nextLine());
+     * System.out.println("Enter Phone Number= ");
+     * newUser.setPhone(input.nextLine());
+     * System.out.println("Enter Password= ");
+     * newUser.setPassword(input.nextLine());
+     * System.out.println("Enter Address= ");
+     * newUser.setAddress(input.nextLine());
+     * app.users.add(newUser);
+     * 
+     * int id;
+     * String name;
+     * String phone;
+     * String Address;
+     * int outletId;
+     * 
+     * 
+     * 
+     */
+
+    /*
+     * int idR = 0;
+     * 
+     * Scanner sc = new Scanner(System.in);
+     * 
+     * System.out.println("\n\n\nEnter CNIC/ID to remove= ");
+     * idR = sc.nextInt();
+     * 
+     * Iterator<Staff> it = app.employees.iterator();
+     * 
+     * int header = 0;
+     * 
+     * while (it.hasNext() && header == 0) {
+     * 
+     * if (it.next().id == idR) {
+     * it.next().id = 0;
+     * it.next().name = null;
+     * it.next().outletId = 0;
+     * it.next().Address = null;
+     * it.next().phone = null;
+     * header = 1;
+     * }
+     * }
+     * if (header == 1) {
+     * System.out.println("\n\n\nEmployee deactivated \n\n\n ");
+     * }
+     * 
+     * else {
+     * System.out.println("\n\n\nEmployee Out Found \n\n\n ");
+     * 
+     * }
+     */
+
+    private static void AllEmployee() {
+        System.out.println("\n\n\nAll Employees: \n\n\n");
+
+        Iterator<Staff> it = app.employees.iterator();
+
+        while (it.hasNext()) {
+            System.out.println("\nName: " + it.next().name);
+            System.out.println("        Id: " + it.next().id);
+        }
+    }
+
+    private static void AllJobs() {
+        System.out.println("\n\n\nAll Jobs: \n\n\n");
+
+        Iterator<Job> it = app.jobs.iterator();
+
+        while (it.hasNext()) {
+            System.out.println("\nID: " + it.next().id);
+            System.out.println("    Description: " + it.next().description);
+            System.out.println("    Date: " + it.next().scheduleDate);
+            System.out.println("    StaffID: " + it.next().staffId);
+        }
+
+    }
+
+    private static void addEmployee() {
+        int id = 0;
+        String name = null;
+        String phone = null;
+        String Address = null;
+        int outletId = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n\n\nEnter CNIC= ");
+        id = sc.nextInt();
+        System.out.println("Enter Name= ");
+        name = sc.nextLine(); // reads string.
+        System.out.println("Enter Phone Number= ");
+        phone = sc.nextLine(); // reads string.
+        System.out.println("Enter Address= ");
+        Address = sc.nextLine(); // reads string.
+        System.out.println("Enter Outlet ID= ");
+        outletId = sc.nextInt();
+
+        Staff newEmployee = new Staff(id, name, phone, Address, outletId);
+        app.employees.add(newEmployee);
+        // System.out.println("Employee Added");
+
+    }
+
+    private static void removeEmployee() {
+        int idR = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n\n\nEnter CNIC/ID to remove= ");
+        idR = sc.nextInt();
+
+        Iterator<Staff> it = app.employees.iterator();
+
+        int header = 0;
+
+        while (it.hasNext() && header == 0) {
+
+            if (it.next().id == idR) {
+                it.next().id = 0;
+                it.next().name = null;
+                it.next().outletId = 0;
+                it.next().Address = null;
+                it.next().phone = null;
+                header = 1;
+            }
+        }
+        if (header == 1) {
+            System.out.println("\n\n\nEmployee deactivated \n\n\n ");
+        }
+
+        else {
+            System.out.println("\n\n\nEmployee Out Found \n\n\n ");
+
+        }
+    }
+
+    /*
+     * 
+     * 
+     * int id;
+     * String description;
+     * int customerId;
+     * int staffId;
+     * String scheduleDate;
+     * int outletId;
+     * 
+     * public Job(int id,String description,int customerId,int staffId,String
+     * scheduleDate,int outletId)
+     * {
+     * setId(id);
+     * setDescription(description);
+     * setCustomerId(customerId);
+     * setStaffId(staffId);
+     * setScheduleDate(scheduleDate);
+     * setOutletId(outletId);
+     * }
+     * 
+     */
+
+    private static void AssignTask() {
+        System.out.println("\n\n\nAssigning task\n\n\n");
+        int id = 0;
+        String description = null;
+        int customerId = 0;
+        int staffId = 0;
+        String scheduleDate = null;
+        int outletId = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n\n\nEnter Job ID= ");
+        id = sc.nextInt();
+        System.out.println("Enter Desprictions= ");
+        description = sc.nextLine(); // reads string.
+        System.out.println("Enter customer ID= ");
+        customerId = sc.nextInt(); // reads string.
+        System.out.println("Enter scheduleDate= ");
+        scheduleDate = sc.nextLine(); // reads string.
+        System.out.println("Enter Outlet ID= ");
+        outletId = sc.nextInt();
+
+        Job newJob = new Job(id, description, customerId, staffId, scheduleDate, outletId);
+
+        app.jobs.add(newJob);
+
+    }
+
+}
 }
